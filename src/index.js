7b1ee481a5884c26eb367e8b24ade261e0df2a2b
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-04-05 23:02:51 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -16,8 +16,19 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-function preload() {
-  this.load.image('study', introImage);
+// function preload() {
+//   this.load.image('study', introImage);
+// }
+function preload ()
+{
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
 }
 
 function create() {
