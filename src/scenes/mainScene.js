f4323d84d67f7851c21c4e185d0d66980d3fa328
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-04-13 02:00:10 +0400

    add hero spritesheet

--- src/scenes/mainScene.js
+++ src/scenes/mainScene.js
@@ -1,4 +1,7 @@
 import 'phaser';
+import heroKnightPic from '../assets/images/hero.png'
+import heroKnightJson from '../assets/images/hero_atlas.json'
+
 
 export default class MainScene extends Phaser.Scene{
   constructor(){
@@ -7,10 +10,14 @@ export default class MainScene extends Phaser.Scene{
 
   preload(){
     console.log('preload');
+    this.load.atlas('hero',heroKnightPic,heroKnightJson)
   }
   create(){
     console.log('create');
-    this.player = new Phaser.Physics.Matter.Sprite(this.matter.world)
+    let f = heroKnightJson.frames[0]
+    console.log(f);
+    this.player = new Phaser.Physics.Matter.Sprite(this.matter.world,20,20,'hero',f.frame)
+    this.add.existing(this.player)
     this.inputKeys = this.input.keyboard.addKeys({
       up:Phaser.Input.Keyboard.KeyCodes.W,
       down:Phaser.Input.Keyboard.KeyCodes.S,
--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,16 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
     ]
   },
   devServer: {
